7dfee7e301a855ba8cf22dd25500521fe01f7f8c
kritomas <Unknown>
2024-03-20 07:47:49 +0100

    Initial commit.

---
+++ DBC.php
@@ -0,0 +1,75 @@
+<?php
+
+class DBC
+{
+	private static $CREDENTIALS = null;
+    public static $SERVER_IP = null;
+    public static $USER = null;
+    public static $PASSWORD = null;
+    public static $DATABASE = null;
+
+    private static $connection = null;
+
+    protected function __construct()
+    {
+    }
+
+    public static function getConnection()
+    {
+		error_reporting(E_ALL);
+		ini_set('display_errors', 'on');
+		if (!self::$CREDENTIALS)
+		{
+			self::$CREDENTIALS = get_object_vars(json_decode(file_get_contents("sql_credentials.json")));
+		   	self::$SERVER_IP = self::$CREDENTIALS["ip"];
+		    self::$USER = self::$CREDENTIALS["user"];
+		    self::$PASSWORD = self::$CREDENTIALS["password"];
+		    self::$DATABASE = self::$CREDENTIALS["database"];
+		}
+
+        if (!self::$connection) {
+            self::$connection = mysqli_connect(self::$SERVER_IP, self::$USER, self::$PASSWORD, self::$DATABASE);
+            if (!self::$connection) {
+                die('Could not connect to DB');
+            }
+        }
+        return self::$connection;
+    }
+
+    public static function closeConnection()
+    {
+        if (self::$connection) {
+            mysqli_close(self::$connection);
+            self::$connection = null;
+        }
+    }
+
+	public static function createDB()
+	{
+		self::getConnection()->query("create database Usero");
+	}
+	public static function initDB()
+	{
+		error_reporting(E_ALL);
+		ini_set('display_errors', 'on');
+		self::getConnection()->query("create table Account (id int primary key AUTO_INCREMENT, username varchar(32) not null unique, password varchar(256) not null);");
+	}
+	public static function delDB()
+	{
+		error_reporting(E_ALL);
+		ini_set('display_errors', 'on');
+		self::getConnection()->query("drop table Account;");
+	}
+
+	public static function select()
+	{
+		error_reporting(E_ALL);
+		ini_set('display_errors', 'on');
+		$query = self::getConnection()->query("select * from Account;");
+		while ($row = $query->fetch_assoc())
+		{
+			var_dump($row);
+			echo "<br>";
+		}
+	}
+}
---
+++ index.php
@@ -0,0 +1,39 @@
+<?php
+
+require './DBC.php';
+
+//DBC::select();
+
+session_start();
+
+echo '
+<html lang="en" dir="ltr">
+	<head>
+		<meta charset="utf-8">
+		<link rel="stylesheet" href="./style.css">
+		<title>Animaldo</title>
+	</head>
+	<body>
+		<header>
+			<a href="register.php">Register</a>
+			<a href="login.php">Log In</a>';
+if (!empty($_SESSION["username"]))
+{
+	echo '<a href="data.php">Data</a>
+	Logged in as <b>' . $_SESSION["username"] . '</b>
+	<a href="handle_logout.php">Log Out</a>
+	';
+}
+echo '
+		</header>
+		<p>
+			WELKAM to Animaldo. Here we make animals and keep track of animals and account and all those weird animaly things.
+		</p>
+		<footer>
+			Animaldo 2024
+		</footer>
+	</body>
+</html>
+';
+
+?>
